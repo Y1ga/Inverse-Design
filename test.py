f5f2f471f5baec669e9d7cfbab479c5652bb7e80
unknown <1002556995@qq.com>
2023-10-15 15:51:54 +0800

    version 0.0

---
+++ test.py
@@ -0,0 +1,7 @@
+import os,sys
+# sys.path.append("W:\\Software\\Lumerical\\v202\\api\\python\\")
+# import lumapi
+# import lumopt
+import numpy as np
+target_T_fwd = lambda wl: np.ones(wl.size)
+x = target_T_fwd()
